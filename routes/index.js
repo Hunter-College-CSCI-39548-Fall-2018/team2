fc97b8183a1231813fe94c7292fcad5cc20cad6c
Maria Mahin <mariamcm90@gmail.com>
2018-11-02 12:53:33 -0400

    Update css

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,18 @@ var router = express.Router();
 
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user });
+    res.render('index', {user: req.user});
 });
 
-router.get('/register', function(req, res) {
-    res.render('register', { });
+router.get('/register', function (req, res) {
+    res.render('register', {});
 });
 
-router.post('/register', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+router.post('/register', function (req, res) {
+    Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
         if (err) {
             //ToDo: Currently just refreshes page if invalid. Add response to this behavior
-            return res.render('register', { account : account });
+            return res.render('register', {account: account});
         }
 
         passport.authenticate('local')(req, res, function () {
@@ -25,16 +25,16 @@ router.post('/register', function(req, res) {
     });
 });
 
-router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+router.get('/login', function (req, res) {
+    res.render('login', {user: req.user});
 });
 
 router.post('/login', passport.authenticate('local',
-    { successRedirect: '/', failureRedirect: '/login', failureFlash: true }
+    {successRedirect: '/', failureRedirect: '/login', failureFlash: true}
 ));
 
 
-router.get('/logout', function(req, res) {
+router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
