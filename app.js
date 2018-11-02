7fded8d308e6635b72aa9820a42be6bca7b8d189
Maria Mahin <mariamcm90@gmail.com>
2018-11-02 12:16:59 -0400

    Update readme

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,9 +28,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 
-
 app.use('/', routes);
-
 
 var Account = require('./models/account');
 passport.use(new LocalStrategy(
@@ -64,15 +61,14 @@ var mongoDB = 'mongodb://127.0.0.1:27017/';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-
-// development error handler. will print stacktrace
+// Development error handler. will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -82,14 +78,4 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
-// production error handler. no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
 module.exports = app;
