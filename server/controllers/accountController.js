8f964894cf964bfed41358d12fcd9be649e2c8d4
erinthered <erinbesswilliams@gmail.com>
2018-12-05 08:40:58 -0500

    tweak home styling

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,56 +1,61 @@
-const Account = require('../models/account');
-const passport = require('passport');
+const Account = require("../models/account");
+const passport = require("passport");
 
-exports.account_home_get = function (req, res) {
-    res.sendStatus(200);
+exports.account_home_get = function(req, res) {
+  res.sendStatus(200);
 };
 
-exports.account_subgoals_get = function (req, res) {
-    if (req.user) {
-        res.redirect('/subgoals');
+exports.account_learn_get = function(req, res) {
+  res.sendStatus(200);
+};
+
+exports.account_subgoals_get = function(req, res) {
+  if (req.user) {
+    res.redirect("/subgoals");
+  }
+};
+
+exports.account_registration_get = function(req, res) {
+  if (req.user) {
+    res.redirect("/");
+  }
+};
+
+exports.account_registration_post = function(req, res) {
+  Account.register(
+    new Account({ username: req.body.username, password: req.body.password }),
+    req.body.password,
+    function(err, account) {
+      if (err || req.body.password !== req.body.re_password) {
+        console.log(err);
+        // ToDo: Currently just refreshes page if invalid. Add response indicating error to user
+      }
+
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/login");
+      });
     }
+  );
 };
 
-exports.account_registration_get = function (req, res) {
-    if (req.user) {
-        res.redirect('/');
-    }
+exports.account_login_post = function(req, res) {
+  res.sendStatus(200);
 };
 
-exports.account_registration_post = function (req, res) {
-    Account.register(new Account({username: req.body.username, password: req.body.password}),
-        req.body.password, function (err, account) {
-            if (err || (req.body.password !== req.body.re_password)) {
-                console.log(err);
-                // ToDo: Currently just refreshes page if invalid. Add response indicating error to user
-            }
-
-            passport.authenticate('local')(req, res, function () {
-                res.redirect('/login');
-            });
-        });
-};
-
-exports.account_login_post = function (req, res) {
+exports.account_login_get = function(req, res) {
+  if (req.user) {
+    res.redirect("/goals");
+  } else {
     res.sendStatus(200);
+  }
 };
 
-exports.account_login_get = function (req, res) {
-    if (req.user) {
-        res.redirect('/goals');
-    } else {
-        res.sendStatus(200);
-    }
-
+exports.account_logout_get = function(req, res) {
+  req.session.destroy();
+  req.logout();
+  res.redirect("/");
 };
 
-exports.account_logout_get = function (req, res) {
-    req.session.destroy();
-    req.logout();
-    res.redirect('/');
-};
-
-
-exports.account_error_get = function (req,res) {
-    res.sendStatus(200);
+exports.account_error_get = function(req, res) {
+  res.sendStatus(200);
 };
