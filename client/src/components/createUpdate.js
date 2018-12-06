7c5cf355629a70ed4f056fda55bc30ee05249625
Kah Hau Yap <kahhauyap@gmail.com>
2018-12-06 06:34:09 -0500

    configure for deploy

--- client/src/components/createUpdate.js
+++ client/src/components/createUpdate.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {post} from 'axios';
+import React, { Component } from 'react';
+import { post } from 'axios';
 
 class createUpdate extends Component {
 
@@ -58,8 +58,7 @@ class createUpdate extends Component {
 
     uploadFile(event) {
         let file = event.target.files[0];
-        alert('file' + file);
-        this.setState({imageValue: file});
+        this.setState({ imageValue: file });
 
         if (file) {
             let data = new FormData();
@@ -73,33 +72,33 @@ class createUpdate extends Component {
                 <form onSubmit={this.handleSubmit} ref={el => this.form = el}>
                     <div className="mdl-textfield mdl-js-textfield">
                         <input className="mdl-textfield__input" name='title'
-                               onChange={this.handleInputChange} value={this.state.title} type="text"/>
+                            onChange={this.handleInputChange} value={this.state.title} type="text" />
                         <label className="mdl-textfield__label">Update Title</label>
                     </div>
                     <div className="mdl-textfield mdl-js-textfield">
                         <input className="mdl-textfield__input" name='description'
-                               type="text" onChange={this.handleInputChange}/>
+                            type="text" onChange={this.handleInputChange} />
                         <label className="mdl-textfield__label">Update Description</label>
                     </div>
                     <div className="mdl-textfield mdl-js-textfield">
                         <input className="mdl-textfield__input file-input" name='img'
-                               onChange={this.handleInputChange}
-                               placeholder={this.state.imageValue.name} type="text" id="uploadFile" readOnly/>
+                            onChange={this.handleInputChange}
+                            placeholder={this.state.imageValue.name} type="text" id="uploadFile" readOnly />
                         <div
                             className="mdl-button mdl-button--primary mdl-button--icon mdl-button--file">
                             <i className="material-icons">
                                 cloud_upload
                             </i>
-                            <input type="file" id="uploadBtn" onChange={this.uploadFile}/>
+                            <input type="file" id="uploadBtn" onChange={this.uploadFile} />
                         </div>
                         <label className="mdl-textfield__label">Update Image Upload</label>
                     </div>
                     <div className="modal-footer">
-                    <input type="submit" value="submit" className='rkmd-btn-toggled'
-                           data-dismiss="modal" onClick={() => {
-                        this.form.dispatchEvent(new Event('submit'))
-                    }} />
-                </div>
+                        <input type="submit" value="submit" className='rkmd-btn-toggled'
+                            data-dismiss="modal" onClick={() => {
+                                this.form.dispatchEvent(new Event('submit'))
+                            }} />
+                    </div>
                 </form>
             </div>
         );
--- client/src/components/goalFeed.js
+++ client/src/components/goalFeed.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from './subgoalsHeader';
 import NavBar from './navbar';
 import Subgoal from './subgoal';
 import Post from './post';
-import {get} from 'axios';
+import { get } from 'axios';
 
 class GoalFeed extends Component {
 
@@ -35,21 +35,17 @@ class GoalFeed extends Component {
 
         this.callApiSubgoals()
             .then(result => {
-                this.setState({subgoals: result.subgoals});
+                this.setState({ subgoals: result.subgoals });
                 feedPosts.push.apply(feedPosts, result.subgoals);
             }).catch(err => console.log(err));
 
 
         this.callApiPosts()
             .then(result => {
-                this.setState({posts: result.posts});
+                this.setState({ posts: result.posts });
                 feedPosts.push.apply(feedPosts, result.posts);
                 let copy = feedPosts.slice(0);
-                copy.sort(function(a,b){return a.created.getTime() - b.created.getTime()});
-
-                for(var i = 0; i < copy.length; i++) {
-                    alert(copy[i].created);
-                }
+                copy.sort(function (a, b) { return a.created.getTime() - b.created.getTime() });
 
                 this.setState({
                     feed: feedPosts
@@ -65,7 +61,8 @@ class GoalFeed extends Component {
         return get('/subgoals/fetch', {
             params: {
                 id: this.props.location.state.goalId
-            }}).then(response => {
+            }
+        }).then(response => {
             return response.data;
         });
 
@@ -75,7 +72,8 @@ class GoalFeed extends Component {
         return get('/posts/fetch', {
             params: {
                 id: this.props.location.state.goalId
-            }}).then(response => {
+            }
+        }).then(response => {
             return response.data;
         });
     };
@@ -90,11 +88,11 @@ class GoalFeed extends Component {
                     this.state.feed.map((post, index) => {
                         if (post.structure === 'Post') {
                             return (<Post key={index} id={post._id} postTitle={post.title}
-                                          postDescription={post.description} postImage={post.img} postDate={post.created}/>);
+                                postDescription={post.description} postImage={post.img} postDate={post.created} />);
                         } else {
                             return (<Subgoal key={index} id={post._id} subgoalTitle={post.title}
-                                             subgoalDescription={post.description}
-                                             completed={post.completed} subgoalDate={post.created}/>);
+                                subgoalDescription={post.description}
+                                completed={post.completed} subgoalDate={post.created} />);
                         }
                     })}
                 </ul>
@@ -104,10 +102,10 @@ class GoalFeed extends Component {
 
     render() {
         return (
-            <div  id ="backgroundFeed" className="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+            <div id="backgroundFeed" className="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
                 <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-                    <NavBar loggedIn={true}/>
-                    <Header goalTitle={this.state.goalTitle} goalId={this.state.goalId}/>
+                    <NavBar loggedIn={true} />
+                    <Header goalTitle={this.state.goalTitle} goalId={this.state.goalId} />
                     <main className="mdl-layout__content">
                         <div className="mdl-layout__tab-panel is-active" id="overview">
                             {this.displayFeed(this.state.feed)}
